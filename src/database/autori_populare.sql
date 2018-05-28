2f41ea5db543b8ba3035086e628cdc59a252b481
lapachitei <lapachitei@bitdefender.com>
2018-05-28 18:55:56 +0300

    Populari

---
+++ src/database/autori_populare.sql
@@ -0,0 +1,9 @@
+insert into authors(name) values('Constantin Chirita');
+insert into authors(name) values('Ioan Slavici');
+insert into authors(name) values('George Calinescu');
+insert into authors(name) values('Liviu Rebreanu');
+insert into authors(name) values('Jojo Mayes');
+insert into authors(name) values('Matt Haig');
+insert into authors(name) values('John Grisham');
+insert into authors(name) values('Anne Frank');
+commit;
